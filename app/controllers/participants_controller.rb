f9b30dcce63b3ef4fc58501a0454942f467cbe42
Marie Sepanik <sepanik.marie@gmail.com>
2022-02-28 17:58:17 +0100

    participants methods

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,8 +1,10 @@
 class ParticipantsController < ApplicationController
   def past_participations
+    @past_participations = Participation.created_before(DateTime.now)
   end
 
   def upcoming_participations
+    @upcoming_participations = Participation.created_after(DateTime.now)
   end
 
   def create
