fab4102eeffecd34bc115cb99ca41ca3a153b289
Maxime Plasse <m20plasse@gmail.com>
2022-03-01 16:28:06 +0100

    roots and policies

--- app/policies/participation_policy.rb
+++ app/policies/participation_policy.rb
@@ -1,4 +1,4 @@
-class ParticipantPolicy < ApplicationPolicy
+class ParticipationPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
